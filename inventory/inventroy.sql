bfd55d632be8159484420f3a0b27c267c9d55a4c
Dkhunk <80922939+DivineDK@users.noreply.github.com>
2024-09-19 13:35:26 +0000

    Update inventroy.sql

--- inventory/inventroy.sql
+++ inventory/inventroy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2024 at 08:54 AM
+-- Generation Time: Sep 08, 2024 at 08:54 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
